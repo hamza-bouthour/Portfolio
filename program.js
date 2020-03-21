264bceaa302d1dd34c69a2179e6f619e1a1de5c4
Hamza Bouthour <h.bouthour@gmail.com>
2020-03-21 01:15:02 -0700

    program.js

--- program.js
+++ program.js
@@ -1,16 +1,26 @@
 let countCLick = 1
 $(document).ready(function(event) {
-    event.preventDefault;
-    event.stopPropagation;
+    
     
 $(".photo-name").on('click', function(){
+    event.preventDefault;
+    event.stopPropagation;
     if (countCLick == 1) {
- $(".informations").removeClass('hidden');  
+ $(".informations").removeClass('hidden'); 
+  $('.photo-name').html('Close window').css('color', '#a0b5d9');
    countCLick = 2;
     }else {
         $(".informations").addClass('hidden'); 
+        $('.photo-name').html('Hamza Bouthour').css('color', 'white');;
         countCLick = 1;
     }
 });
+// $(document).on('click', 'body *', function(){
+//     if (countCLick == 2) {
+//     $(".informations").addClass('hidden'); 
+//     countCLick = 1;
+//     }
+    
+// });
 
 });
